a8423bbfcef86c1b3efa2e94fff8e0ddec1f786c
Luqman Hadi <luqmanhadi.s@live.iium.edu.my>
2023-01-25 21:10:32 +0800

    fixed user input error, printArray method implemented

--- src/main/java/com/mycompany/practise/Practise.java
+++ src/main/java/com/mycompany/practise/Practise.java
@@ -13,16 +13,18 @@ public class Practise {
 
     public static void main(String[] args) {
         int[] unsorted = new int[10];
+        System.out.println("Input 10 numbers in any order: ");
         Scanner input = new Scanner(System.in);
         for(int i = 0; i < unsorted.length; i++){
             unsorted[i] = input.nextInt();
         }
         bubbleSort(unsorted);
-        System.out.println("Input 10 numbers in any order: ");
+        printArray(unsorted);
+
     }
     public static void bubbleSort(int[] unsorted){
         for(int j = 0; j < unsorted.length; j++){
-            for(int i = 0; i < unsorted.length; i++){
+            for(int i = 0; i < unsorted.length-1; i++){
                 if(unsorted[i] > unsorted[i+1]){
                     int temp = unsorted[i];
                     unsorted[i] = unsorted[i+1];
@@ -31,5 +33,9 @@ public class Practise {
             }      
         }
     }
-    public static void printArray()
+    public static void printArray(int[] unsorted){
+        for(int i = 0; i < unsorted.length; i++){
+            System.out.print(unsorted[i]+" ");
+        }
+    }
 }
